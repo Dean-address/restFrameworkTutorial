49d8b5046eb1605f9edfd1db8af11ae5112465f3
Alabi Khalid <alabikhalid.12@gmail.com>
2024-10-21 21:54:28 +0100

    explored Request and Responses with rest_framework status code (End Of Tutorial2)

--- snippets/urls.py
+++ snippets/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from snippets import views
+from rest_framework.urlpatterns import format_suffix_patterns
 
 urlpatterns = [
     path(r"snippets/", views.snippet_list),
     path(r"snippets/<int:pk>/", views.snippet_detail),
 ]
+urlpatterns = format_suffix_patterns(urlpatterns)
